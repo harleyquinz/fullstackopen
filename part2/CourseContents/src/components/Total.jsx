8a0ac8fcca4a0184454d107823ec3c94fe73f54d
Bruna Fischer <Bruna@Brunas-MacBook-Air.local>
2024-05-16 20:35:04 -0300

    Part2 + CourseContent

---
+++ part2/CourseContents/src/components/Total.jsx
@@ -0,0 +1,10 @@
+const Total = (props) => {
+    const course = props.course;
+    return (
+      <>
+        <p>Number of exercises {course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises}</p>
+      </>
+    )
+}
+
+export default Total
